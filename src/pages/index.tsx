9fbbc6bedea7cdc9f942dfcc63a4861cfbb5f4d6
Waylon Peng <waylonpeng@gmail.com>
2021-03-18 15:00:33 -0700

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default function Home(props) {
+  return (
+    <div>
+      <Link to="/hey/check/this/out">Click me.</Link>
+      {props.params.key}
+    </div>
+  );
+};
+
+// TODO:
+// render viewer component using createPage
+// pass urls and such as pageContext prop
